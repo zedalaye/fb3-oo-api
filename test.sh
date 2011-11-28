1ee98371e817309222e38d388ecd7b2dcee68895
Pierre Yager <pierre@levosgien.net>
2011-11-28 23:43:27 +0100

    I would probably have better to create a Makefile...

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-FIREBIRD_SOURCE=../firebird/src/include
+FIREBIRD_ROOT=../firebird/src
+FIREBIRD_SOURCE=${FIREBIRD_ROOT}/include
 FIREBIRD_RELEASE=/opt/fb3cs
 TEST=firetest
 
-LC_ALL=C g++ -Wall -I${FIREBIRD_SOURCE} -o ${TEST} -fPIC ${TEST}.cpp ${FIREBIRD_RELEASE}/lib/libfbclient.so
+LC_ALL=C g++ -Wall -I${FIREBIRD_SOURCE} -c -fPIC ${TEST}.cpp
+LC_ALL=C g++ -Wall -I${FIREBIRD_SOURCE} -o ${TEST} -fPIC ${TEST}.o ${FIREBIRD_RELEASE}/lib/libfbclient.so
 
 if [[ $? -eq 0 ]]; then
   ./$TEST
