e95c03e1200bfe2a2f88c4df22c98d862d3917e0
Pierre Yager <pierre@levosgien.net>
2011-11-28 23:42:21 +0100

    Improved printStatus() Try IProvider->createDatabase() and IAttachment->drop()

--- firetest.cpp
+++ firetest.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "stdio.h"
+#include "string.h"
 #include "ibase.h"
 #include "firebird/Interface.h"
 #include "firebird/Plugin.h"
@@ -15,13 +17,19 @@
 using namespace std;
 using namespace Firebird;
 
-void printStatus(const IStatus* status)
+int printStatus(const IStatus* status)
 {
   const ISC_STATUS* status_array = status->get();
   cout << "Status[]=" << status_array[0];
   for (int i = 1; i < ISC_STATUS_LENGTH; i++)
      cout << "," << status_array[i];
   cout << " (isSuccess=" << status->isSuccess() << ")\n";
+  if (!status->isSuccess())
+  {
+    cout << "Error Occured:\n";
+    isc_print_status(status_array);
+  } 
+  return status->isSuccess();
 }
 
 int main()
@@ -42,6 +50,27 @@ int main()
   cout << "Before Init "; printStatus(status);
   status->init();
   cout << "After Init "; printStatus(status);
+   
+  char dpb[48];
+  int i=0;
+  dpb[i++] = isc_dpb_version1;
+  dpb[i++] = isc_dpb_user_name;
+  dpb[i++] = (char)6;
+  strncpy(&(dpb[i]), "SYSDBA", 6);
+  i += 6;
+  dpb[i++] = isc_dpb_password;
+  dpb[i++] = (char)9;
+  strncpy(&(dpb[i]), "masterkey", 9);  
+  i += 9;
+     
+  cout << "Create Database\n";
+  IAttachment* attachment = provider->createDatabase(status, "localhost:/tmp/test.fb3", i, reinterpret_cast<const unsigned char*>(dpb));
+  if (printStatus(status))
+  {
+    cout << "Drop Database\n";
+    attachment->drop(status);
+    printStatus(status);
+  }
 
   return 0;
 }
